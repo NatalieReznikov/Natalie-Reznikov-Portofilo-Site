3f2efee146cdf17eeb3bc8607c3b924a879de421
yakking <reznyakov@gmail.com>
2020-07-25 17:04:08 -0400

    Added foundational content and style to navigation bar.

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="NavBar">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name : "NavBar"
+}
+</script>
+
+<style scoped>
+#NavBar{
+    background-color: #282C34;
+    width: 100%;
+    height: 100px;
+}
+</style>
