1a9f815d520267592b8446e4ea0ad0ab3bcbec65
yakking <reznyakov@gmail.com>
2020-07-25 17:04:49 -0400

    Added foundational home page section.

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="HomePage"> </div>
+</template>
+
+<script>
+export default {
+  name : "HomePage"
+}
+</script>
+
+<style scoped>
+#HomePage{
+  background-image: url("../assets/New background v2.png");
+  background-size: contain;
+  width: 100%;
+  height: 100%;
+}
+</style>
