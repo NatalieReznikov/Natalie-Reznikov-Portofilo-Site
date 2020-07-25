901b7c9c7047d94b4ad1e72e2dfe8352c652eb62
yakking <reznyakov@gmail.com>
2020-07-25 17:04:34 -0400

    Added carousel with slides to main content section.

---
+++ src/components/MainContent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="MainContent">
+        <carousel :per-page="1" :paginationEnabled="false" height="100%">
+            <slide>
+                <HomePage></HomePage>
+            </slide>
+        </carousel>
+    </div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+import HomePage from './HomePage.vue';
+
+export default {
+  name : "MainContent",
+  components: {
+    Carousel,
+    Slide,
+    HomePage
+  }
+};
+</script>
+
+<style scoped>
+#MainContent{
+    width: 100%;
+    height: calc(100% - 100px);
+}
+.VueCarousel{
+    height: 100%;
+}
+
+</style>
+
+<style>
+.VueCarousel-wrapper{
+    height: 100%;
+}
+.VueCarousel-inner{
+    height: 100% !important;
+}
+</style>
