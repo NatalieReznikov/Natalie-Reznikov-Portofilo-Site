3fb8800b7ed7c2d1aeca945db839da5057127a8b
Jenny Xu <jxu0053@gmail.com>
2020-07-29 20:41:12 -0400

    Put gradient background Put Natalie picture Put Title and Subtitle.

--- src/components/sections/HomePage.vue
+++ src/components/sections/HomePage.vue
@@ -1,5 +1,14 @@
 <template>
-  <div id="HomePage"></div>
+  <div id="HomePage">
+    <div class = "homepage">
+      <h1> <i> <shift>Natalie </shift> Reznikov</i> </h1>
+      <h2>An Engineer in the body of a Biologist </h2>
+    </div>
+
+    <div class = "pfp">
+      <img src = "../../assets/natalie picture_nobg_bw.png">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,8 +19,44 @@ export default {
 
 <style scoped>
 #HomePage{
-  background-color: rgba(0, 0, 29, 0.192);
   width: 100%;
   height: 100%;
+  background-image: radial-gradient(circle at 75% 50%, rgba(0,0,0,0.95) 30%, rgba(0,0,0,0));
 }
+
+shift{
+  margin-left: 13px;
+  display: block;
+  margin-bottom: 10px;
+}
+
+.pfp{
+  position: absolute;
+  left: 60%;
+
+}
+img{
+  width: 550px;
+  height: auto;
+}
+
+
+.homepage {
+    position: absolute;
+    left: 25%;
+    top: 35%;
+  }
+  .homepage h1 {
+    position: static;
+    color: white;
+    font-size: 60px;
+    text-align: left;
+  }
+  .homepage h2 {
+    position: relative;
+    right: 50px;
+    top: -10px;
+    font-size: 35px;
+  }
+
 </style>
