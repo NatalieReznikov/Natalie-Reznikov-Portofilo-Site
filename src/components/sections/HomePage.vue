ceb193658f2c269d2e7d6554588748d69d575b35
yakking <reznyakov@gmail.com>
2020-08-18 17:00:25 -0400

    Updated home page to use an image.

--- src/components/sections/HomePage.vue
+++ src/components/sections/HomePage.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="HomePage">
-    <div class = "homepage">
-      <h1> <i> <shift>Natalie </shift> Reznikov</i> </h1>
-      <h2>An Engineer in the body of a Biologist </h2>
-    </div>
-
-    <div class = "pfp">
-      <img src = "../../assets/natalie picture_nobg_bw.png">
+    <div class="homepage">
+      <img src="./../../assets/home page v3 with E.png"/>
     </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name : "HomePage"
 }
@@ -21,42 +18,16 @@ export default {
 #HomePage{
   width: 100%;
   height: 100%;
+  position: relative;
+  display: block;
   background-image: radial-gradient(circle at 75% 50%, rgba(0,0,0,0.95) 30%, rgba(0,0,0,0));
 }
 
-shift{
-  margin-left: 13px;
-  display: block;
-  margin-bottom: 10px;
-}
-
-.pfp{
+.homepage{
   position: absolute;
-  left: 60%;
-
+  display: inline-block;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%,-50%,0);
 }
-img{
-  width: 550px;
-  height: auto;
-}
-
-
-.homepage {
-    position: absolute;
-    left: 25%;
-    top: 35%;
-  }
-  .homepage h1 {
-    position: static;
-    color: white;
-    font-size: 60px;
-    text-align: left;
-  }
-  .homepage h2 {
-    position: relative;
-    right: 50px;
-    top: -10px;
-    font-size: 35px;
-  }
-
 </style>
