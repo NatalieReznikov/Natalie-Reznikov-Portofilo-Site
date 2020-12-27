5953e224fc95b9e1094cad197b7117a10b05f9b5
Jacob Reznikov <reznyakov@gmail.com>
2020-12-27 02:59:06 +0000

    Fixed sizes of headers in contact info.

--- src/components/sections/ContactInfo.vue
+++ src/components/sections/ContactInfo.vue
@@ -7,16 +7,17 @@
             Teaching office: McConnell Engineering Building <br>
             Research lab: 141 du Président-Kennedy Avenue, 5th floor
         </h5>
-        <h1>
+        <br>
+        <h3>
             Jacob Reznikov - Web Development, Web Design
-        </h1>
+        </h3>
         <h5> 
             Github: <a href="https://github.com/yakking">https://github.com/yakking</a> <br>
             Email: <a href="mailto:yakov.reznikov@mail.mcgill.ca">yakov.reznikov at mail.mcgill.ca</a> 
         </h5>
-        <h1>
+        <h3>
             Jenny Xu - Extra Coding Help
-        </h1>
+        </h3>
         <h5> 
             Github: <a href="https://github.com/jxu0053">https://github.com/jxu0053</a> <br>
             Email: <a href="mailto:yakov.reznikov@mail.mcgill.ca">jenny.xu at mail.mcgill.ca</a> 
